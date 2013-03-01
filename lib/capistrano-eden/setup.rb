143284aaa1bd6551d10e51526d0e0e6ad0c69b71
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2013-03-01 17:06:19 -0300

    My dirtree.

--- lib/capistrano-eden/setup.rb
+++ lib/capistrano-eden/setup.rb
@@ -1,37 +1,56 @@
-require File.dirname(__FILE__) + '/../capistrano-eden' if ! defined?(CapistranoAbril)
+require File.dirname(__FILE__) + '/../capistrano-eden' if ! defined?(CapistranoEden)
 
-CapistranoAbril.with_configuration do
+CapistranoEden.with_configuration do
 
   namespace :deploy do
 
+    desc '(setup.rb): This task is overwritten by eden:setup'
+    task :setup do; end;
+
+  end # namespace
+
+  namespace :eden do
+
     desc <<-DESC
+      First run: create initial directories for all hosts.
+
       (setup.rb): create initial directories for all hosts.
+
       For a specific host:
+
         $ cap HOSTS=new.server.com deploy:setup
+
+      The directory tree is the one defined by eden:defaults.
+      For more information: use 'cap -e eden:defaults'
+
     DESC
     task :setup, :except => { :no_release => true } do
 
+      eden.defaults
 
       cmds = []
       dirs = [ "#{base_path}"          \
-             , "#{logs_path}"          \
              , "#{deploy_to}"          \
              , "#{releases_path}"      \
              , "#{releases_path}/1"    \
-             , "#{shared_path}"        \
-             , "#{shared_path}/config" \
              , "#{config_path}"        \
+             , "#{log_path}"           \
+             , "#{tmp_path}"           \
              ]
 
-      dirs.each do |d|
-          cmds << "#{sudo} mkdir -p                #{d}"
-          cmds << "#{sudo} chmod g+w               #{d}"
-          cmds << "#{sudo} chown #{user}:#{ugroup} #{d}"
-      end
+      # extra dirs inside #{shared_path}
+      dirs += shared_children.map { |d| File.join(shared_path, d.split('/').last) }
+
+      # do it
+      cmds << "#{sudo} mkdir -p #{ dirs.join(' ') }"
+      cmds << "#{sudo} chmod -R g+w #{base_path}" if fetch(:group_writable, true)
+      cmds << "#{sudo} chown -R #{user}:#{group} #{base_path}"
       cmds << "ln -nfs #{releases_path}/1 #{deploy_to}/current"
-      run cmds.join(';'), :pty => true
+
+      run cmds.join('; '), :pty => true
+
     end
-    before "deploy:setup" , "deploy:setup_vars"
+    before "deploy:setup" , "eden:setup"
 
   end # namespace
 
